ed6957253ac76521d9cfd1e63259b20a199879e1
FORLUV <FORLUV@FORLUV-PC>
2014-06-02 16:14:40 +0800

    add ConfigService, SocketAddressToString etc.
    Signed-off-by: FORLUV <FORLUV@FORLUV-PC>

---
+++ com.semeureka.frame/src/main/java/com/semeureka/frame/misc/SocketAddressToString.java
@@ -0,0 +1,26 @@
+package com.semeureka.frame.misc;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+import org.apache.mina.transport.serial.SerialAddress;
+import org.apache.mina.transport.serial.SerialAddress.DataBits;
+import org.apache.mina.transport.serial.SerialAddress.FlowControl;
+import org.apache.mina.transport.serial.SerialAddress.Parity;
+import org.apache.mina.transport.serial.SerialAddress.StopBits;
+import org.springframework.core.convert.converter.Converter;
+
+public class SocketAddressToString implements Converter<String, SocketAddress> {
+	@Override
+	public SocketAddress convert(String source) {
+		if (source.matches("\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+")) {
+			int sep = source.indexOf(":");
+			return new InetSocketAddress(source.substring(0, sep), Integer.parseInt(source
+					.substring(sep + 1)));
+		} else if (source.toUpperCase().matches("COM\\d+")) {
+			return new SerialAddress(source.toUpperCase(), 38400, DataBits.DATABITS_8,
+					StopBits.BITS_1, Parity.NONE, FlowControl.NONE);
+		}
+		throw new IllegalArgumentException();
+	}
+}
